c025056470703d5040fd9792c9a7b3a2740e4a6b
Denis Sanchez Leyva <denisijcu266@gmail.com>
2026-07-24 08:11:05 -0400

    ready to deploy en railway

--- src/main.py
+++ src/main.py
@@ -114,7 +114,7 @@ async def unhandled_exception_handler(request: Request, exc: Exception):
     )
 
 
-@app.get("/", tags=["Infrastructure"])
+@app.get("/x", tags=["Infrastructure"])
 async def root():
     """Identifica el servicio.
 
